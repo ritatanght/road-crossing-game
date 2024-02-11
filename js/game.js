3866e0f5d76a869e2770a5667f47033f3b777c52
Rita Tang <doggymint@gmail.com>
2024-02-10 22:36:54 -0500

    Load background and player, display background on scene

--- js/game.js
+++ js/game.js
@@ -1,7 +1,27 @@
-// 1. create a new scene - where all the actions take place
+// create a new scene - where all the actions take place
 let gameScene = new Phaser.Scene("Game");
 
-// 2. set the config of the game
+// load assets
+gameScene.preload = function () {
+  // load.image(name, path)
+  this.load.image("background", "assets/background.png");
+  this.load.image("player", "assets/player.png");
+};
+
+// called once after the preload ends
+gameScene.create = function () {
+  let bg = this.add.sprite(0, 0, "background"); // this has to match the name in preload above
+  // 1st method: change the origin to to the top-left corner instead of the default 'center'
+  // bg.setOrigin(0, 0);
+
+  // 2nd method: place sprite in the center
+  // get width and height from system config
+  let gameW = this.sys.game.config.width;
+  let gameH = this.sys.game.config.height;
+  bg.setPosition(gameW / 2, gameH / 2);
+};
+
+// set the config of the game
 let config = {
   // how will phaser render this game: WebGL, CanvasAPI
   type: Phaser.AUTO, // Phaser will use WebGL if available, if not will use Canvas
@@ -10,5 +30,5 @@ let config = {
   scene: gameScene,
 };
 
-// 3. create a new game, pass the config
+// create a new game, pass the config
 let game = new Phaser.Game(config);
