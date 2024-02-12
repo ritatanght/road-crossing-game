337be38c4513f5a70d70a71c64afecc224775ede
ritatanght <doggymint@gmail.com>
2024-02-12 16:12:58 -0500

    Add group of enemies and end game on collision

--- js/game.js
+++ js/game.js
@@ -41,18 +41,39 @@ gameScene.create = function () {
   this.goal = this.add.sprite(gameW - 80, gameH / 2, "goal");
   this.goal.setScale(0.6);
 
-  // enemy
-  this.enemy = this.add.sprite(120, gameH / 2, "enemy");
-  this.enemy.flipX = true;
-  this.enemy.setScale(0.6);
+  // enemy group
+  this.enemies = this.add.group({
+    key: "enemy",
+    repeat: 5,
+    setXY: {
+      // x & y for the first element
+      x: 90,
+      y: 100,
+      // the separation between the subsequent sprites
+      stepX: 80,
+      stepY: 20,
+    },
+  });
 
-  // set enemy speed
-  let direction = Math.random() < 0.5 ? 1 : -1;
-  let speed =
-    this.enemyMinSpeed +
-    Math.random() * (this.enemyMaxSpeed - this.enemyMinSpeed);
-  this.enemy.speed = direction * speed;
-  console.log(speed);
+  // setting scale for all group elements
+  Phaser.Actions.ScaleXY(this.enemies.getChildren(), -0.4, -0.4);
+
+  // set flipX and speed, the function will be called for each one of our entity
+  Phaser.Actions.Call(
+    this.enemies.getChildren(),
+    function (enemy) {
+      // flip enemy
+      enemy.flipX = true;
+
+      // set enemy speed
+      let direction = Math.random() < 0.5 ? 1 : -1;
+      let speed =
+        this.enemyMinSpeed +
+        Math.random() * (this.enemyMaxSpeed - this.enemyMinSpeed);
+      enemy.speed = direction * speed;
+    },
+    this // pass in the context of the current scene
+  );
 };
 
 // update is called up to 60 times/second
@@ -72,15 +93,30 @@ gameScene.update = function () {
     return;
   }
 
-  // enemy movement
-  this.enemy.y += this.enemy.speed;
-  // check we haven't passed min or max Y
-  let conditionUp = this.enemy.speed < 0 && this.enemy.y <= this.enemyMinY;
-  let conditionDown = this.enemy.speed > 0 && this.enemy.y >= this.enemyMaxY;
+  // get enemies
+  let enemies = this.enemies.getChildren();
 
-  // if we passed the upper or lower limit, reverse
-  if (conditionUp || conditionDown) {
-    this.enemy.speed *= -1;
+  for (let i = 0; i < enemies.length; i++) {
+    // enemy movement
+    enemies[i].y += enemies[i].speed;
+
+    // check we haven't passed min or max Y
+    let conditionUp = enemies[i].speed < 0 && enemies[i].y <= this.enemyMinY;
+    let conditionDown = enemies[i].speed > 0 && enemies[i].y >= this.enemyMaxY;
+
+    // if we passed the upper or lower limit, reverse
+    if (conditionUp || conditionDown) {
+      enemies[i].speed *= -1;
+    }
+
+    // check enemy overlap
+    let enemyRect = enemies[i].getBounds();
+    if (Phaser.Geom.Intersects.RectangleToRectangle(playerRect, enemyRect)) {
+      console.log("Game over!");
+      // restart the scene
+      this.scene.restart();
+      return;
+    }
   }
 };
 
